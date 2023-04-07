ea4aacd2003761eeb59a0b0fa6ede362d1cafb74
dinez-k <dineshspm18@gmail.com>
2023-04-07 12:25:37 +0530

    latestg chages

--- src/main/java/com/scrapify/bidservice/BidServiceApplication.java
+++ src/main/java/com/scrapify/bidservice/BidServiceApplication.java
@@ -3,35 +3,32 @@ package com.scrapify.bidservice;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Bids API", version = "1.0", description = "Bids Information"))
-public class BidServiceApplication  implements ApplicationRunner {
+public class BidServiceApplication {
 
 	@Autowired
 	private KafkaTemplate<String, String> kafkaTemplate;
 
-
-	public void sendMessage(String msg) {
-		kafkaTemplate.send("scrapify", msg);
-	}
+//
+//	public void sendMessage(String msg) {
+//		kafkaTemplate.send("scrapify", msg);
+//	}
 	public static void main(String[] args) {
 		SpringApplication.run(BidServiceApplication.class, args);
 	}
 
-	@KafkaListener(topics = "scrapify", groupId = "group-id")
-	public void listen(String message) {
-		System.out.println("Received Messasge in group - group-id: " + message);
-	}
-
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		sendMessage("Hi Welcome to Spring For Apache Kafka");
-	}
+//	@KafkaListener(topics = "scrapify", groupId = "group-id")
+//	public void listen(String message) {
+//		System.out.println("Received Messasge in group - group-id: " + message);
+//	}
+//
+//	@Override
+//	public void run(ApplicationArguments args) throws Exception {
+//		sendMessage("Hi Welcome to Spring For Apache Kafka");
+//	}
 }
